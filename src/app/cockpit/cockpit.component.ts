54b37910a578656f89a36252e473d89dcef0f082
Maksim Teplyy <maksim.teplyy@teamvoy.com>
2017-07-13 18:36:24 +0300

    Initial commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
+
+  // NOTE: Different types of sending html information using
+  onAddBlueprint(name: string) {
+    this.blueprintCreated.emit({
+      serverName: name,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
+}
---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,80 @@
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  Input,
+  ViewEncapsulation,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild
+} from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
+  @Input('srvElement') element: {type: string, name: string, content:string};
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
+
+  constructor() {
+    console.log('constructor called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+    console.log('ngOnChanges called!');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called!');
+    console.log(`Text content here: ${this.header.nativeElement.textContent}`);
+    console.log(`Text content of paragraph here: ${this.paragraph.nativeElement.textContent}`);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+    console.log(`Text content of paragraph here: ${this.paragraph.nativeElement.textContent}`);
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
+
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+    console.log(`Text content here: ${this.header.nativeElement.textContent}`);
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
+  }
+}
